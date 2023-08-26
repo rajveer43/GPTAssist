a15bc439fd64cc4a3a5bbc22dadf2c217411f16a
rajveer43 <rajveer.rathod1301@gmail.com>
2023-08-26 21:24:52 +0530

    g

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(
     name="PersonalGPT",
     version="0.1",
-    description="Your Personal GPT powered assistance, do whatever you want to do with your voice commands",
-    url="https://github.com/Swarno-coder/PersonalGPT",
-    packages=["PersonalGPT"],
-    author="Swarnodip Nag",
-    author_email="official.swarnodipnag@gmail.com",
+    description="Your Personal GPT powered assistance, do whatever you want to do with your voice commands, and have fun add your mates as well",
+    url="https://github.com/rajveer43/GPTAssist",
+    packages=["GPTAssist"],
+    author="Rajveer Rathod",
+    author_email="rajveer.rathod1301@gmail.com",
 )
